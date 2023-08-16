bfbb298cf5f36e8716e89b84c6cab7fef3b963a9
hui.cheng <hui.cheng@naturali.io>
2023-08-16 14:51:31 +0800

    refactor

--- rag-serve.py
+++ rag-serve.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import json
+
 import os
 import sys
 import logging
-from aiohttp import web
-from pybars import Compiler
 import openai
 
-openai.api_key = "sk-***"  # your openai api key you registered on the openai.
-
-from langchain.embeddings import HuggingFaceEmbeddings
-
-from llama_index import StorageContext, ServiceContext, load_index_from_storage
-from llama_index.embeddings import LangchainEmbedding
+from aiohttp import web, ClientSession
+from pybars import Compiler
 from llama_index import set_global_service_context
-from llama_index.response.schema import RESPONSE_TYPE
+from llama_index.embeddings import LangchainEmbedding
+from llama_index import StorageContext, ServiceContext, load_index_from_storage
+from langchain.embeddings import HuggingFaceEmbeddings
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 routes = web.RouteTableDef()
+
 
 @routes.get("/")
 async def hello(_: web.Request):
@@ -28,10 +25,7 @@ async def hello(_: web.Request):
 
 
 def conversation(prompt, turns):
-    res = [{
-        "role": "system",
-        "content": prompt
-    }]
+    res = [{"role": "system", "content": prompt}]
     res.extend(turns)
     return res
 
@@ -39,34 +33,32 @@ def conversation(prompt, turns):
 # curl -v -d 'input=中国有多大' http://127.0.0.1:8080/query
 @routes.post("/query")
 async def query(request: web.Request):
-    req = await request.post()
-    prompt = req.get("prompt")
+    req = await request.json()
+    turns = req.get("turns", [])
+    prompt = req.get("prompt", "")
     if len(prompt) == 0:
         prompt = request.app['prompt']
-
-    turns = req.get("turns")
-    size = len(turns)
     if len(turns) == 0:
         return web.json_response({"errMsg": f'input type is not str'})
-    if turns[size - 1].role != "user":
+    if turns[-1].get("role", "") != "user":
         return web.json_response({"errMsg": f'last turn is not from user'})
 
-    user_input = turns[size - 1].text
+    user_input = turns[-1].get("content", "")
 
     retriever = request.app['engine']
 
     context = retriever.retrieve(user_input)
     compiler = request.app['compiler']
     template = compiler.compile(prompt)
-    new_prompt = template({
-        "query": query,
-        "context": context})
+    new_prompt = template({"query": user_input, "context": context})
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation(new_prompt, turns),
-        temperature=0  # Try to as deterministic as possible.
-    )
+    async with ClientSession(trust_env=True) as session:
+        openai.aiosession.set(session)
+        response = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            messages=conversation(new_prompt, turns),
+            temperature=0  # Try to as deterministic as possible.
+        )
 
     resp = {"reply": response.choices[0].message["content"]}
     return web.json_response(resp)
@@ -77,11 +69,11 @@ def init_app(index):
     app.add_routes(routes)
     app['engine'] = index.as_retriever()
     app['compiler'] = Compiler()
-    app['prompt'] =  "We have provided context information below. \n" \
-                     "---------------------\n"\
-                     "{{context}}"\
-                     "\n---------------------\n"\
-                     "Given this information, please answer the question: {{query}}\n"
+    app['prompt'] = "We have provided context information below. \n" \
+        "---------------------\n"\
+        "{{context}}"\
+        "\n---------------------\n"\
+        "Given this information, please answer the question: {{query}}\n"
     return app
 
 
@@ -108,5 +100,4 @@ if __name__ == "__main__":
 
     storage_context = StorageContext.from_defaults(persist_dir=p)
     index = load_index_from_storage(storage_context)
-
     web.run_app(init_app(index))
